6cabe5af51562e284f387da769a546cef02050a4
Pratham Sharma <prrrathm@gmail.com>
2021-10-21 00:26:55 +0530

    another changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 
 const name = 'George Bush';
 const element = <h1>Hello, {name}</h1>;
