60eec0e42abf50d6305c77c91a42d593ae2906fc
Pratham Sharma <prrrathm@gmail.com>
2021-10-21 00:26:30 +0530

    some changes

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const name = 'George Bush';
+const element = <h1>Hello, {name}</h1>;
 
 ReactDOM.render(
   <React.StrictMode>
+    {element}
+    <h1>HELLO</h1>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
