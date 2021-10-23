328adeb836e25b145c8a97327797916003882f92
Pratham Sharma <prrrathm@gmail.com>
2021-10-23 21:02:27 +0530

    removal and renewal

--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Game from './component/game';
-
-const user = {
-  firstname : 'Acharya',
-  lastname : 'Narendra'
-}
-
-function formatname(user){
-  return user.firstname + ' and  ' + user.lastname;
-}
-
-const name = 'George Bush';
-const element = <h1>Hello, {formatname(user)}</h1>;
 
 ReactDOM.render(
-  element,
-  document.getElementById('root')
+  <>
+  HELLO
+  </>
 );
 // reportWebVitals();
